b0078d25b2123c701f815d09aed4b7c915fdc526
Izumo1982 <84167062+Izumo1982@users.noreply.github.com>
2021-07-15 20:28:36 +0900

    Add files via upload

---
+++ fizzbuzz_yield.rb
@@ -0,0 +1,11 @@
+﻿def yie
+  num = yield
+	str = 'Fizz'      if num % 3 == 0
+	str = 'Buzz'      if num % 5 == 0
+	str = 'Fizz Buzz' if num % 15 == 0
+	str ||= num.to_s
+end
+
+
+1.upto 100 do |num| p yie {num} end
+
